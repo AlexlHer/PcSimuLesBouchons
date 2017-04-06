3c0c19b6e5ce98e55edcd833175a3628f5a8bc69
Alexandre <alexandre.alex10@hotmail.fr>
2017-04-06 17:06:18 +0200

    v0.3

--- Route.h
+++ Route.h
@@ -10,15 +10,21 @@ class Route
 	public:
 		Route(int nbVoiture);
 		Route(int nbVoiture, int limite, int tailleRoute, int frein);
-		void ajouterVoiture(int numVoiture);
-		void enleverVoiture(int numVoiture);
+		void ajouterVoiture();
+		void ajouterVoiture(Voiture v);
+		void ajouterVoiture(Voiture v, int pos);
+		void enleverVoiture();
+		void enleverVoiture(Voiture v);
+		void placementAleatoire(Voiture v);
 		void placementAleatoireDepart();
-		void tempsPlus(int temps);
+		void tempsPlus(int temps, int modele);
 		std::vector<int> tabAffiche();
-		int tailleRoute();
+		int getTailleRoute();
+		void setTailleRoute(int taille);
 		void setLimiteVitesse(int vit);
 		int espaceAvant(int imaticulation);
 		void setProbaFrein(int pourcent);
+		int getProbaFrein();
 
 	private:
 		std::vector<Voiture> tabVoiture;
