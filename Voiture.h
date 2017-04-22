a70c84c55cf8b672b466b29c8913fe43931c1faa
Alexandre <alexandre.alex10@hotmail.fr>
2017-04-23 01:05:03 +0200

    Version 1.0

--- Voiture.h
+++ Voiture.h
@@ -1,17 +1,27 @@
+// --------------------------------
+// Auteur : Alexandre l'Heritier
+// PcSimuLesBouchons v1.0 : Classe Voiture
+// --------------------------------
+
 #ifndef DEF_VOITURE
 #define DEF_VOITURE
- 
+
 class Voiture
 {
 	public:
+		// Constructeurs
 		Voiture();
 		Voiture(int numero);
 		Voiture(int numero, int vitess);
+
+		// Accesseurs
 		void setImatriculation(int numero);
 		void setVitesse(int vitess);
 		int getImatriculation();
 		int getVitesse();
+
 	private:
+		// Attributs
 		int vitesse;
 		int imatriculation;
 };
