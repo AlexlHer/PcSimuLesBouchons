c725ff79c0b7cc64d30bfdd1795799406bed0ce6
Alexandre <alexandre.alex10@hotmail.fr>
2017-04-23 01:04:41 +0200

    Version 1.0

--- Voiture.cpp
+++ Voiture.cpp
@@ -1,43 +1,71 @@
 // --------------------------------
 // Auteur : Alexandre l'Heritier
-// Projet Bouchon v0.6 : Classe Voiture
+// PcSimuLesBouchons v1.0 : Classe Voiture
 // --------------------------------
 
 #include "Voiture.h"
 
+/*
+Constructeur d'une voiture "vide".
+*/
 Voiture::Voiture()
 {
 	vitesse = -1;
 	imatriculation = -1;
 }
 
+/*
+Constructeur d'une voiture avec une immatriculation particulière et une vitesse nulle.
+@param numero L'immatriculation voulu.
+*/
 Voiture::Voiture(int numero)
 {
 	vitesse = 0;
 	imatriculation = numero;
 }
 
+/*
+Constructeur d'une voiture avec une immatriculation particulière et une vitesse particulière.
+@param numero L'immatriculation voulu.
+@param vitess La vitesse voulu.
+*/
 Voiture::Voiture(int numero, int vitess)
 {
 	vitesse = vitess;
 	imatriculation = numero;
 }
 
+/*
+On defini une immatriculation pour la voiture.
+@param numero Le numero d'immatriculation à définir.
+*/
 void Voiture::setImatriculation(int numero)
 {
 	imatriculation = numero;
 }
 
+/*
+On defini une vitesse pour la voiture.
+@param vitess La vitesse à définir.
+*/
 void Voiture::setVitesse(int vitess)
 {
 	vitesse = vitess;
 }
 
+/*
+Renvoi l'immatriculation de la voiture.
+@return L'immatriculation de la voiture.
+*/
 int Voiture::getImatriculation()
 {
 	return imatriculation;
 }
 
+/*
+Renvoi la vitesse de la voiture.
+@return La vitesse de la voiture.
+*/
 int Voiture::getVitesse()
 {
 	return vitesse;
