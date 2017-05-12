392f15edb66fd297521a728828b9a598ea431e78
Alexandre <alexandre.alex10@hotmail.fr>
2017-05-12 23:44:02 +0200

    Version 3.0

--- Route.h
+++ Route.h
@@ -1,6 +1,6 @@
 // --------------------------------
 // Auteur : Alexandre l'Heritier
-// PcSimuLesBouchons v2.0 : Classe Route
+// PcSimuLesBouchons v3.0 : Classe Route
 // --------------------------------
 
 #ifndef DEF_ROUTE
@@ -12,51 +12,51 @@
 
 class Route
 {
-	public:
-		// Constructeurs
-		Route();
-		Route(int nbVoiture);
-		Route(int nbVoiture, int tailleRoute);
+public:
+	// Constructeurs
+	Route();
+	Route(int nbVoiture);
+	Route(int nbVoiture, int tailleRoute);
 
-		// Méthodes
-		void ajouterVoiture();
-		void ajouterVoiture(int imatriculation);
-		void ajouterVoiture(Voiture v);
-		void ajouterVoiture(Voiture v, int pos);
-		void enleverVoiture();
-		void enleverVoiture(int imatriculation);
-		void tempsPlus(int temps, int modeleNV);
-		std::vector<Voiture> tabAffiche();
-		std::vector<Voiture> getTabVoiture();
-		int espaceAvant(int imaticulation);
+	// Méthodes
+	void ajouterVoiture();
+	void ajouterVoiture(int imatriculation);
+	void ajouterVoiture(Voiture v);
+	void ajouterVoiture(Voiture v, int pos);
+	void enleverVoiture();
+	void enleverVoiture(int imatriculation);
+	void tempsPlus(int temps, int modeleNV);
+	std::vector<Voiture> tabAffiche();
+	std::vector<Voiture> getTabVoiture();
+	int espaceAvant(int imaticulation);
 
-		// Accesseurs
-		int getTailleRoute();
-		void setTailleRoute(int taille);
-		void setLimiteVitesse(int vit);
-		int getLimiteVitesse();
-		void setProbaFrein(int pourcent);
-		int getProbaFrein();
-		void setProbaResteArret(int pourcent);
-		int getProbaResteArret();
-		int getNbVoiture();
+	// Accesseurs
+	int getTailleRoute();
+	void setTailleRoute(int taille);
+	void setLimiteVitesse(int vit);
+	int getLimiteVitesse();
+	void setProbaFrein(int pourcent);
+	int getProbaFrein();
+	void setProbaResteArret(int pourcent);
+	int getProbaResteArret();
+	int getNbVoiture();
 
-	private:
-		// Attributs
-		std::vector<Voiture> tabVoiture;	// Contient toutes les voitures présente sur la route.
-		std::vector<Voiture> tabRoute;		// Contient la route (avec les voitures de tabVoiture et des "voiture vide".
-		int limiteDeVitesse;				// Contient la limite de vitesse de la route.
-		int probaFrein;						// Contient la proba de freinage des voitures.
-		int probaResteArret;				// Contient la proba de redemarrage des voitures.
+private:
+	// Attributs
+	std::vector<Voiture> tabVoiture;	// Contient toutes les voitures présente sur la route.
+	std::vector<Voiture> tabRoute;		// Contient la route (avec les voitures de tabVoiture et des "voiture vide".
+	int limiteDeVitesse;				// Contient la limite de vitesse de la route.
+	int probaFrein;						// Contient la proba de freinage des voitures.
+	int probaResteArret;				// Contient la proba de redemarrage des voitures.
 
-		// Méthodes
-		bool determineFrein();
-		void actualiserTabVoiture();
-		void modele(int nom_modele);
-		bool determineResteArret();
-		bool intIn(int i, std::vector<int> dans);
-		void placementAleatoire(Voiture v);
-		void placementAleatoireDepart();
+										// Méthodes
+	bool determineFrein();
+	void actualiserTabVoiture();
+	void modele(int nom_modele);
+	bool determineResteArret();
+	bool intIn(int i, std::vector<int> dans);
+	void placementAleatoire(Voiture v);
+	void placementAleatoireDepart();
 };
 
 #endif
